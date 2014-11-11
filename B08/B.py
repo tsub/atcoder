a892e499f4507062112c7466d9f3dc019ec1b265
Robinia <nor.known.to.life.51122@gmail.com>
2014-11-11 21:11:16 +0900

    B08Bの無駄な処理の削除

--- B08/B.py
+++ B08/B.py
@@ -1,9 +1,9 @@
 from collections import Counter
 
-n = int(raw_input())
+n = input()
 sn = []
 
 for i in range(n):
     sn.append(raw_input())
 
-print Counter(sn).most_common()[0][0]
+print Counter(sn).most_common(1)[0][0]
